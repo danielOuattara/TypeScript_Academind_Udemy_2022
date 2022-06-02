6ec4abed46614df68874e51261fd5f979f5d7caf
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-02 23:22:44 +0200

    starting Typscript basics, to continue next time

---
+++ 02_TypeScript_Basics/01_Course_Project_Setup/app.ts
@@ -0,0 +1,83 @@
+/* Initial 
+--------------*/
+
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+// const number1 = 5;
+// const number2 = "2.8";
+
+// const result = add(number1, number2);
+
+// console.log(result);
+
+/* Typsecript help during development, it does not run on runtime 
+-----------------------------------------------------------------
+ - Javascript => dynamic type ,evaluated on runtine
+ - TypeScript => static ,evaluated before runtime
+
+ CAUTION: The core primitive types in TypeScript are all lowercase!
+*/
+
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+
+// const number1 = 5;
+// const number2 = 2.8;
+
+// const result = add(number1, number2);
+
+// console.log(result);
+
+/* ----------------------------------------------- */
+
+/* Working with number, string, boolean */
+
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   if (showResult) {
+//     console.log(phrase, n1 + n2);
+//   } else {
+//     return n1 + n2;
+//   }
+// }
+
+// const number1 = 5;
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = "The result is : ";
+// add(number1, number2, printResult, resultPhrase);
+
+/* ----------------------------------------------- */
+
+/* Type Assignment & Type Inference */
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  if (showResult) {
+    console.log(phrase, n1 + n2);
+  } else {
+    return n1 + n2;
+  }
+}
+
+let number1: number = 5; // Not useful & redondant: type inference already does the job
+// let number1 = 5; // good V
+
+// let number2; // type 'any'
+// number2 = 2.5;  // OK
+// number2= "2.5"  // OK
+
+let number2: number;
+number2 = 2.5 // Good V
+number2 = "2.5" // Incorrect  X
+const printResult = true;
+
+const resultPhrase_0 = "The result is : "; 
+console.log(typeof resultPhrase_0) // string
+
+let resultPhrase = "The result is : "; // type automatically recognized as string
+resultPhrase = 23 // Incorrect
+
+
+add(number1, number2, printResult, resultPhrase);
