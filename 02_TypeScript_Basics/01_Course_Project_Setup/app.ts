cdc0750d73106ac9f5d572e6a157124533fe736f
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-07 22:38:26 +0200

    tuples, OK

--- 02_TypeScript_Basics/01_Course_Project_Setup/app.ts
+++ 02_TypeScript_Basics/01_Course_Project_Setup/app.ts
@@ -8,6 +8,8 @@
 // const number1 = 5;
 // const number2 = "2.8";
 
+// concatenation problem can occurs
+
 // const result = add(number1, number2);
 
 // console.log(result);
@@ -53,31 +55,165 @@
 
 /* Type Assignment & Type Inference */
 
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  if (showResult) {
-    console.log(phrase, n1 + n2);
-  } else {
-    return n1 + n2;
-  }
-}
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   if (showResult) {
+//     console.log(phrase, n1 + n2);
+//   } else {
+//     return n1 + n2;
+//   }
+// }
 
-let number1: number = 5; // Not useful & redondant: type inference already does the job
-// let number1 = 5; // good V
+// let number1: number = 5;
+// // Not useful & redondant:  native type inference in TypeScript already does the job
 
-// let number2; // type 'any'
-// number2 = 2.5;  // OK
-// number2= "2.5"  // OK
+// // let number1 = 5; // good V
 
-let number2: number;
-number2 = 2.5 // Good V
-number2 = "2.5" // Incorrect  X
-const printResult = true;
+// // let number2; // type 'any'
+// // number2 = 2.5;  // OK
+// // number2= "2.5"  // OK
 
-const resultPhrase_0 = "The result is : "; 
-console.log(typeof resultPhrase_0) // string
+// let number2: number;
+// number2 = 2.5 // Good V
+// // number2 = "2.5" // Incorrect X, why value is string ?
+// const printResult = true;
 
-let resultPhrase = "The result is : "; // type automatically recognized as string
-resultPhrase = 23 // Incorrect
+// const resultPhrase_0 = "The result is : ";
+// console.log(typeof resultPhrase_0) // string
+
+// let resultPhrase = "The result is : "; // type inference automatically recognized as string
+// // resultPhrase = 23 // Incorrect, variable  was previously recognized as string, so why does it have a number value ?
+
+// add(number1, number2, printResult, resultPhrase);
+
+/* ----------------------------------------------- */
+
+/* Objects Type */
+
+// // best way
+// const person = {
+//   name: "Daniel",
+//   age: 38,
+// };
+
+// console.log(person);
+
+// console.log(person.lastname);
+// // Incorrect, TypeScript knows that trher is no property called latname in person object
+
+// // autocompletion OK using the dot
+
+// // Any object has an object type
+
+// /*
+// const person = {
+//   name: string,
+//   age: number,
+// };
+
+// */
+
+// // Below is restrictive,
+// const person2: object = {
+//   name: "Daniel",
+//   age: 38,
+// };
+// // console.log(person2.);
+// // No auto completion
+
+// // Below is restrictive, as above
+// const person3: {} = {
+//   name: "Daniel",
+//   age: 38,
+// };
+
+// // console.log(person2.);
+// // No auto completion
+
+// // Below is restrictive, as above
+// const person4: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Daniel",
+//   age: 38,
+// };
+
+// // console.log(person2.);
+// // No auto completion
+
+/* ----------------------
+Nested Objects & Types */
+
+// Of course object types can also be created for nested objects.
+// Let's say you have this JavaScript object:
+
+// const product = {
+//   id: "abc1",
+//   price: 12.99,
+//   tags: ["great-offer", "hot-and-new"],
+//   details: {
+//     title: "Red Carpet",
+//     description: "A great carpet - almost brand-new!",
+//   },
+// };
+
+// // This would be the type of such an object:
+
+// // {
+// //   id: string;
+// //   price: number;
+// //   tags: string[];
+// //   details: {
+// //     title: string;
+// //     description: string;
+// //   }
+// // }
+
+// // So you have an object type in an object type so to say.
+
+/* ---------------------------------------------------------
+Arrays Types */
+
+// const person = {
+//   name: "Daniel",
+//   age: 38,
+//   hobbies: ["sports", "cooking", "flying"],
+// };
+
+// let favoriteActivities: string[];
+
+// // favoriteActivities = "sports"; // Incorrect, expected array of string, not a string variable
+
+// favoriteActivities = ["sports"]; // Correct, received an array of string
+
+// // favoriteActivities = ["sports", 1]; // Incorrect, array of string only
+
+// let favoriteActivities2: any[];
+
+// favoriteActivities2 = ["sports", 1]; // Correct, array of any
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby, "- index : ", person.hobbies.indexOf(hobby));
+// }
+
+/* ---------------------------------------------------------
+Tuples Types */
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Daniel",
+  age: 38,
+  hobbies: ["sports", "cooking", "flying"],
+  role: [2, "author"],
+};
 
 
-add(number1, number2, printResult, resultPhrase);
+person.role.push("admin") // push() is ONLY accepted in Tuple
+// person.role =[0, "admin","user") // X Incorrect, can't altere Tuple
+// person.role[1] = 10 // Incorrect, typle can't be altered
+
+console.log(person)
