14ce2fd1c6a30a197e699063116cd60c8ef93974
danielOuattara <daniel.ouattara@gmx.com>
2022-06-21 23:28:37 +0200

    Start : tsc compiler configuration file

---
+++ 03_TypeScript_Compiler_Configuration/src/app.ts
@@ -0,0 +1,7 @@
+const userName = "Daniel";
+console.log("Sending...");
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", () => {
+  console.log("Clicked ! ");
+});
