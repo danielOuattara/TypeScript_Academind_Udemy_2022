8f6e28495a8c019fa9187c4de887df7529aca2f0
danielOuattara <daniel.ouattara@gmx.com>
2022-06-21 23:25:05 +0200

    deleted parts

--- 01_Getting_Started/02_Course_Project_Setup/app.ts
+++
@@ -1,5 +0,0 @@
-console.log('Your code goes here...');
-console.log('Your code goes here... Again');
-console.log('Your code goes here... with lite-server');
-console.log('Your code goes here... with lite-server.. doe sit work ?');
-console.log('Your code goes here... with lite-server.. doe sit work ? Yes It does work, but I prefer  tsc -w filename.ts command');
