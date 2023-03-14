d603cffb45e3f55c8df8327adb0dddcc87245ecd
danielOuattara <daniel.ouattara@gmx.com>
2023-03-14 11:40:32 +0100

    type casting

---
+++ 06_Advanced_Types/src/_04_type_casting.ts
@@ -0,0 +1,19 @@
+const paragraph = document.querySelector("p")!; // over me
+
+paragraph.innerHTML = "Hello";
+
+const paragraph2 = document.getElementById("message-output"); // over me
+
+const userInput2 = document.getElementById("user-input"); // over me
+
+//----------------------------------------
+
+/*  use type casting to be more specific */
+
+// method 1
+const paragraph3 = <HTMLParagraphElement>document.getElementById("message"); // over me
+const userInput3 = <HTMLInputElement>document.getElementById("user-input"); // over me
+
+// method 2
+const paragraph4 = document.getElementById("message") as HTMLParagraphElement; // over me
+const userInput4 = document.getElementById("user-input") as HTMLInputElement; // over me
