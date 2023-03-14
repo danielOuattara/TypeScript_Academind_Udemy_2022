6165503deee6af2bcb04fce0f00b43ea1ed6eba7
danielOuattara <daniel.ouattara@gmx.com>
2023-03-14 11:40:21 +0100

    discriminated unions

---
+++ 06_Advanced_Types/src/_03_discriminated_unions.ts
@@ -0,0 +1,38 @@
+console.log("-------");
+
+/* discriminated union
+-----------------------
+- helps with type guards
+- works well with objects  
+*/
+
+interface Bird {
+  type: "bird"; // help to discriminates
+  year: "2003";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse"; // help to discriminates
+  gallopSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function movingAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.gallopSpeed;
+      break;
+    default:
+      speed = 0;
+  }
+  console.log("Moving with speed :" + speed);
+}
+
+movingAnimal({ type: "bird", flyingSpeed: 300, year: "2003" });
+movingAnimal({ type: "horse", gallopSpeed: 64 });
