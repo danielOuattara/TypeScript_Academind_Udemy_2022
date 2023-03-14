7c8b7c0db8026c55f8b75ca1d27e72a787937028
danielOuattara <daniel.ouattara@gmx.com>
2023-03-14 11:39:55 +0100

    intersection types

---
+++ 06_Advanced_Types/src/_01_intersection_types.ts
@@ -0,0 +1,51 @@
+// Code goes here!
+
+/* Intersection :  & = intersection operator
+-----------------*/
+
+//----------------------------- using Types
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const employee: ElevatedEmployee = {
+  name: "Daniel",
+  privileges: ["start server", "delete old requests"],
+  startDate: new Date("1983-11-15"),
+};
+
+//--------------------------------- using interface
+interface Admin1 {
+  name: string;
+  privileges: string[];
+}
+
+interface Employee1 {
+  name: string;
+  startDate: Date;
+}
+
+// type ElevatedEmployee1 =  Admin1 & Employee1;
+// OR
+interface ElevatedEmployee1 extends Admin1, Employee1 {}
+
+const employee1: ElevatedEmployee1 = {
+  name: "Daniel",
+  privileges: ["start server", "delete old requests"],
+  startDate: new Date("1983-12-15"),
+};
+
+//---------------------------------------
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
