987a62f0dcb7922df938513c1f28dbdf339d7916
danielOuattara <daniel.ouattara@gmx.com>
2023-03-14 11:41:21 +0100

    nullish coalescing

---
+++ 06_Advanced_Types/src/_09_nullish_coalescing.ts
@@ -0,0 +1,14 @@
+/* nullish coalescing 
+-----------------------
+
+*/
+
+const message = null;
+const userMessage = message ?? "No Input";
+console.log(userMessage);
+
+//------------------------
+
+const country = undefined;
+const userCountry = country ?? "Country Unknown";
+console.log(userCountry);
