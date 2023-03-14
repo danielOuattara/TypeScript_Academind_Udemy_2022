8ca453c0e9babe45d46c81200da19ae2906a029c
danielOuattara <daniel.ouattara@gmx.com>
2023-03-14 16:10:05 +0100

    06 generic class

---
+++ 07_Generics/src/_06_generics_class.ts
@@ -0,0 +1,50 @@
+/* _06_generics_class.ts
+-------------------------- 
+
+We can design generic every where needed:
+- in class definition
+- inside some method
+- etc...
+*/
+
+// this class is best working with primitives types, not object
+class ArrayStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    console.log([...this.data]);
+  }
+}
+
+//----------------------------------------------------
+
+const textStorage = new ArrayStorage<string>();
+
+textStorage.addItem("Daniel");
+textStorage.addItem("toto");
+textStorage.removeItem("toto");
+
+textStorage.getItems();
+
+//----------------------------------------------------
+
+const numberStorage = new ArrayStorage<number>();
+
+numberStorage.addItem(10);
+numberStorage.getItems();
+
+//----------------------------------------------------
+
+const numberStringStorage = new ArrayStorage<number | string>();
+
+numberStringStorage.addItem(10);
+numberStringStorage.addItem("John Doe");
+numberStringStorage.getItems();
