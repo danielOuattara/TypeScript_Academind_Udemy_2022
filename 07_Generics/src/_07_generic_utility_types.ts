1d0dfe7e82311da417a23a5a38290be16807b4a1
danielOuattara <daniel.ouattara@gmx.com>
2023-03-14 16:10:19 +0100

    07 generic utility types

---
+++ 07_Generics/src/_07_generic_utility_types.ts
@@ -0,0 +1,28 @@
+/* _07_generic_utility_types.ts
+--------------------------------- */
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completedUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  completedUntil: Date,
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completedUntil = completedUntil;
+
+  return courseGoal as CourseGoal;
+}
+
+// ---------------------------------------------
+
+// names is non modifiable
+const names: Readonly<string[]> = ["Max", "Anna"];
+
+// ---------------------------------------------
