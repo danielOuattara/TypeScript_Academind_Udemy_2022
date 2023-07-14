74b06252e22058282bfee43b52d0b3335ebd23ed
danielOuattara <daniel.ouattara@gmx.com>
2023-07-14 22:42:21 +0200

    08 decorators: 02 decorator factory

---
+++ 08_decorators_meta_programming/src/02_working_with_decorator_factory.ts
@@ -0,0 +1,22 @@
+export {};
+
+// Decorator factory
+function Logger(userName: string) {
+  return function (constructor: Function) {
+    console.log("Logging : ", userName);
+    console.log(constructor.toString());
+  };
+}
+
+@Logger("John Doe")
+class Person {
+  name = "Julie";
+  constructor() {
+    console.log("Creating a person object");
+  }
+}
+
+const person = new Person();
+console.log(person);
+
+// IMPORTANT : Decorators functions are executed on class definition, but not on class instantiation
