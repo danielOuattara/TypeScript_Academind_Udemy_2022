d66c8b3523c8283e5832771c21ec4db49160fd4d
danielOuattara <daniel.ouattara@gmx.com>
2023-07-14 22:47:11 +0200

    08 decorators: 10 auto binding decorators return

---
+++ 08_decorators_meta_programming/src/10_creating_an_autobind_decorator.ts
@@ -0,0 +1,51 @@
+export {};
+
+/* 
+
+Decorators are naturally executed when the class logic is defined;
+but their logic can allow for them to be executed on class instantiation
+*/
+
+function AutoBind(
+  target: any,
+  methodName: string | Symbol,
+  descriptor: PropertyDescriptor,
+) {
+  console.log(" --- Method Decorator --- ");
+  console.log("target = ", target);
+  console.log("MethodName = ", methodName);
+  console.log("method descriptor = ", descriptor);
+
+  const originalMethod = descriptor.value;
+  const adjustableDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: true,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+      return boundFunction;
+    },
+  };
+  return adjustableDescriptor;
+}
+
+class Printer {
+  message = "This works !";
+  showMessage_1() {
+    console.log(this.message);
+  }
+
+  @AutoBind
+  showMessage_2() {
+    console.log(this.message);
+  }
+}
+
+const printer_1 = new Printer();
+
+const button = document.querySelector("button") as HTMLButtonElement;
+
+button.addEventListener("click", () => {
+  printer_1.showMessage_1();
+});
+
+button.addEventListener("click", printer_1.showMessage_2);
