99523d2d1fd3c806beb582dc4c215452d46dc4bc
danielOuattara <daniel.ouattara@gmx.com>
2023-12-25 21:02:50 +0100

    08: 11  revisited +  logic updated

--- 08_decorators_meta_programming/src/11_validation_with_decorators.ts
+++ 08_decorators_meta_programming/src/11_validation_with_decorators.ts
@@ -2,7 +2,7 @@ export {};
 
 interface ValidatorConfig {
   [property: string]: {
-    [validableProp: string]: string[];
+    [validableProp: string]: string[]; // --> ["required, positive"]
   };
 }
 
@@ -10,8 +10,6 @@ const registeredValidators: ValidatorConfig = {};
 
 // decorator: targeting a field element
 function Required(target: any, propName: string) {
-  console.log(target);
-
   registeredValidators[target.constructor.name] = {
     ...registeredValidators[target.constructor.name],
     [propName]: [
@@ -32,6 +30,7 @@ function PositiveNumber(target: any, propName: string) {
   };
 }
 
+//
 function validate(obj: any) {
   const objValidatorConfig = registeredValidators[obj.constructor.name];
   if (!objValidatorConfig) {
@@ -39,6 +38,8 @@ function validate(obj: any) {
   } else {
     let isValid = true;
     for (const prop in objValidatorConfig) {
+      console.log("prop = ", prop);
+
       for (const validator of objValidatorConfig[prop]) {
         switch (validator) {
           case "required":
@@ -50,7 +51,7 @@ function validate(obj: any) {
         }
       }
     }
-    return true;
+    return isValid;
   }
 }
 
@@ -66,24 +67,64 @@ class Course {
   }
 }
 
-const courseForm = document.querySelector("form")!;
+const courseForm = document.querySelector("form")! as HTMLFormElement;
+const titleInputElement = document.getElementById("title") as HTMLInputElement;
+const priceInputElement = document.getElementById("price") as HTMLInputElement;
+
+//
+
 courseForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const titleInputElement = document.getElementById(
-    "title",
-  ) as HTMLInputElement;
-  const priceInputElement = document.getElementById(
-    "price",
-  ) as HTMLInputElement;
-
   const title = titleInputElement.value;
-  const price = Number(priceInputElement.value);
+  const price = parseFloat(priceInputElement.value);
 
   const course_1 = new Course(title, price);
   console.log(course_1);
 
+  console.log("validate(course_1) = ", validate(course_1));
+
   if (!validate(course_1)) {
-    alert("Invalid input, please try again");
+    return alert("Invalid input, please try again");
   }
 });
+
+
+
+/* 
+
+    const config: { [input: string]: string[] } = {};
+     
+    const addValidator = (input: string, type: string) => {
+      config[input] = config[input] 
+        ? [...config[input], type] 
+        : [type];
+    }
+     
+    const Required = (_: any, input: string) => addValidator(input, 'required');
+    const Maxlength = (_: any, input: string) => addValidator(input, 'maxlength');
+    const Positive = (_: any, input: string) => addValidator(input, 'positive');
+     
+    const validate = (course: any) =>  
+      Object.entries(config).every(([input, types]) => 
+        types.every(type => 
+          type === 'required' && course[input] || 
+          type === 'positive' && course[input] > 0 ||
+          type === 'maxlength' && course[input].length < 5
+        )
+      )
+     
+    class Course {
+      
+      @Required @Maxlength title: string;
+      @Required @Positive price: number;
+     
+      constructor(title: string, price: number) {
+        this.title = title;
+        this.price = price;
+      }
+     
+    }
+
+
+*/
