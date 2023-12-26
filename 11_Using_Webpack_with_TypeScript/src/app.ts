01a319ef6481ed9698b5fe15aa64f6dbade11488
danielOuattara <daniel.ouattara@gmx.com>
2023-12-26 19:30:58 +0100

    11  01 renamed folder

--- 11_Using_Webpack_with_TypeScript/src/app.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectInput } from "./components/project_inputs.js";
-import { ProjectList } from "./components/project_list.js";
-import { ProjectStatusEnum } from "./models/project.js";
-
-new ProjectInput();
-new ProjectList(ProjectStatusEnum.ACTIVE);
-new ProjectList(ProjectStatusEnum.FINISHED);
