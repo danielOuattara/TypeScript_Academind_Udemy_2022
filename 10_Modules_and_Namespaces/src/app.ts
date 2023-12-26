2c85aea5ac38bdf579b1070d00abe69d229e6af4
danielOuattara <daniel.ouattara@gmx.com>
2023-12-26 18:28:53 +0100

    relocated

--- 10_Modules_and_Namespaces/src/app.ts
+++
@@ -1,8 +0,0 @@
-///<reference path="./components/project_inputs.ts" />
-///<reference path="./components/project_list.ts" />
-
-namespace App {
-  new ProjectInput();
-  new ProjectList(ProjectStatusEnum.ACTIVE);
-  new ProjectList(ProjectStatusEnum.FINISHED);
-}
