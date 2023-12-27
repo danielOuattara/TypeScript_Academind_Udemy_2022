20ee0b1356e75e42282d12e4fefc8e337583b814
danielOuattara <daniel.ouattara@gmx.com>
2023-12-27 03:02:55 +0100

    13 setting Google API Key + use Axios to fetch Coordinates

--- 13_Project_Select_and_Share_App_Goolge_Map/src/app.ts
+++ 13_Project_Select_and_Share_App_Goolge_Map/src/app.ts
@@ -1,13 +1,42 @@
-// Code goes here!
+import axios from "axios";
 
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "to_be_defined";
+
+type ResType = {
+  results: {
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      };
+    };
+  }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+//
 
 function searchAddress(event: SubmitEvent) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   //send request to google API
+  axios
+    .get<ResType>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress,
+      )}&key=${GOOGLE_API_KEY}`,
+    )
+    .then((res) => {
+      console.log(res);
+      if (!res.data.status) {
+        throw new Error(`${res.status} ${res.statusText}`);
+      }
+      const coordinates = res.data.results[0].geometry.location;
+    })
+    .catch((error) => console.log(error));
 }
 
 form.addEventListener("submit", searchAddress);
