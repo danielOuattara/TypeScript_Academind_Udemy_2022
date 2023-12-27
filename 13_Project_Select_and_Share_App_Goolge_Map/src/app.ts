da7b6ccdcaeba3f98002adfdf15dbb7b0002c85f
danielOuattara <daniel.ouattara@gmx.com>
2023-12-27 01:40:16 +0100

    13 getting user input

--- 13_Project_Select_and_Share_App_Goolge_Map/src/app.ts
+++ 13_Project_Select_and_Share_App_Goolge_Map/src/app.ts
@@ -1,1 +1,13 @@
 // Code goes here!
+
+const form = document.querySelector("form")! as HTMLFormElement;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddress(event: SubmitEvent) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  //send request to google API
+}
+
+form.addEventListener("submit", searchAddress);
